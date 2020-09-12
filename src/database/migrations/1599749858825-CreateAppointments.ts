6f79880fe978e76071363d11e80f2cc67af8cebe
Anderson Wesley <andersonwbezerra@hotmail.com>
2020-09-12 08:27:42 -0400

    GoBarber - Relacionamento nos Models

--- src/database/migrations/1599749858825-CreateAppointments.ts
+++ src/database/migrations/1599749858825-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1599749858825 implements MigrationInterfa
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
--- src/models/Appointments.ts
+++ src/models/Appointments.ts
@@ -1,4 +1,6 @@
-import {Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn} from 'typeorm';
+import {Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn, JoinTable} from 'typeorm';
+
+import User from './Users';
 
 @Entity('appointments')
 class Appointment{
@@ -6,7 +8,11 @@ class Appointment{
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({name: 'provider_id'})
+  provider: User;
   
   @Column('time with time zone')
   date: Date; 
