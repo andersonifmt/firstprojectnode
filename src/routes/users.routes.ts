1f8c4830723b07c8107a01036d33cfbb7c957fca
Anderson Wesley <andersonwbezerra@hotmail.com>
2020-09-17 07:59:52 -0400

    GoBarber - Upload de Imagens

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,15 @@
 import {Router} from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import UserMap from '../mappers/userMap';
 import createUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response)=>{
   try{
@@ -22,6 +29,11 @@ usersRouter.post('/', async (request, response)=>{
   } catch(err){
     return response.status(400).json({ error: err.message});
   }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'),async(request, response)=>{
+  console.log(request.file);
+  return response.json({ ok: true});
 });
 
 export default usersRouter;
