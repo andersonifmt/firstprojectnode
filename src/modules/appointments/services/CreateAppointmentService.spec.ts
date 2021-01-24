ced40b6d2e3d0ae06d3950833df83a8541fdca3f
Anderson Wesley <andersonwbezerra@hotmail.com>
2021-01-24 06:56:08 -0400

    Nivel 04: Gobarber:Criando o primeiro teste

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,20 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentServices from './CreateAppointmentService';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('Should be able to create a new appointment.', () => {
-    expect(1+2).toBe(3);
+  it('Should be able to create a new appointment.', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const CreateAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const appointment = await CreateAppointment.execute({
+      date: new Date(),
+      provider_id: '12323',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12323');
   });
 
   // it('Should not be able to create two appointments on the same time.', () => {
