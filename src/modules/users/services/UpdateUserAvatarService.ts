d926af52d6689cc49adacfa384fed307729bb05f
Anderson Wesley <andersonwbezerra@hotmail.com>
2021-01-25 07:57:06 -0400

    Nivel 04: Gobarber:Provider de storage

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { injectable, inject } from 'tsyringe';
 
 import IUsersRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import AppError from '@shared/erros/AppError';
 
 import User from '../infra/typeorm/entities/User';
@@ -17,7 +18,10 @@ interface IRequest{
 class UpdateUserAvatarService{
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ){}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User>{
@@ -29,14 +33,12 @@ class UpdateUserAvatarService{
     }
 
     if(user.avatar){
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if(userAvatarFileExists){
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
-    user.avatar = avatarFilename;
+
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
